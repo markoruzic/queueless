22a61e2eff82504e6d81d76ff5e586f4280c3665
Marko Ruzic <marko.ruzic01@gmail.com>
2026-04-23 15:06:34 +0200

    Add search and category filter functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,44 @@
+import { useState } from 'react'
 import LocationCard from '../components/LocationCard'
 import { locations } from '../data/locations'
 
 function Home() {
+  const [search, setSearch] = useState('')
+  const [category, setCategory] = useState('All')
+
+  const filteredLocations = locations.filter((location) => {
+    const matchesSearch = location.name
+      .toLowerCase()
+      .includes(search.toLowerCase())
+
+    const matchesCategory =
+      category === 'All' || location.category === category
+
+    return matchesSearch && matchesCategory
+  })
+
   return (
     <main>
       <h1>QueueLess</h1>
-      <p>Pronađi lokaciju i provjeri čekanje.</p>
 
-      {locations.map((location) => (
+      <input
+        type="text"
+        placeholder="Pretraži lokaciju..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <select
+        value={category}
+        onChange={(e) => setCategory(e.target.value)}
+      >
+        <option value="All">Sve</option>
+        <option value="Pošta">Pošta</option>
+        <option value="Banka">Banka</option>
+        <option value="Ljekarna">Ljekarna</option>
+      </select>
+
+      {filteredLocations.map((location) => (
         <LocationCard key={location.id} location={location} />
       ))}
     </main>

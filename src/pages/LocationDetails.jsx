54abf79907297d2cdf5a65e02ba843fcd7b2ae85
Marko Ruzic <marko.ruzic01@gmail.com>
2026-04-23 15:15:15 +0200

    Add dynamic location details page

--- src/pages/LocationDetails.jsx
+++ src/pages/LocationDetails.jsx
@@ -1,8 +1,34 @@
+import { Link, useParams } from 'react-router-dom'
+import { locations } from '../data/locations'
+
 function LocationDetails() {
+  const { id } = useParams()
+  const location = locations.find((item) => item.id === Number(id))
+
+  if (!location) {
+    return (
+      <main className="details-page">
+        <h1>Lokacija nije pronađena</h1>
+        <Link to="/" className="back-link">
+          Povratak na početnu
+        </Link>
+      </main>
+    )
+  }
+
   return (
-    <main>
-      <h1>Detalji lokacije</h1>
-      <p>Ovdje će biti prikaz čekanja, prijava korisnika i dodatne analize.</p>
+    <main className="details-page">
+      <Link to="/" className="back-link">
+        ← Povratak
+      </Link>
+
+      <section className="details-card">
+        <p className="details-category">{location.category}</p>
+        <h1>{location.name}</h1>
+        <p className="details-wait-time">
+          Procijenjeno čekanje: {location.waitTime} min
+        </p>
+      </section>
     </main>
   )
 }
